e71155fade97f53cd45fad3ed8b5d518b89c9002
Anna Nguyen <anna.nguyen42@gmail.com>
2025-05-12 12:47:57 -0400

    update file name resume

--- src/components/Sections/Resume.jsx
+++ src/components/Sections/Resume.jsx
@@ -118,7 +118,7 @@ export default function Resume() {
   return (
     <div className="max-w-4xl mx-auto p-4">
       <div className="flex justify-end mt-4 mb-4">
-        <a href="/resume.pdf" download="hanh_nguyen_resume_may_2025.pdf">
+        <a href="/hanh_nguyen_resume_2025.pdf" download>
           <Button
             text="Download PDF"
             icon={<i className="fas fa-download"></i>}
